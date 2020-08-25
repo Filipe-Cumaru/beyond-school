525874787753f70ef60564b12f15f2a3a28dbb5d
Filipe Antonio Cumaru Silva Alves <filipe-antonio.cumaru-silva-alves@etu.utc.fr>
2020-08-25 18:29:21 -0300

    Add remaining content from day 2 tutorial.

--- dia-2/beyond-school-tutorial/src/App.vue
+++ dia-2/beyond-school-tutorial/src/App.vue
@@ -1,19 +1,60 @@
 <template>
   <v-app>
     <v-main>
-      <v-app-bar>
-        <v-btn icon x-large>
-          <v-icon x-large>mdi-chevron-left</v-icon>
+      <v-app-bar fixed>
+        <v-btn x-large icon>
+          <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
-        Filipe Cumaru
+        {{ name }}
+      </v-app-bar>
+      <div class="messages-container">
+        <div v-for="(message, i) in messages" :key="i">
+          <MessageCard :nameProp='name' :messageProp='message'></MessageCard>
+        </div>
+      </div>
+      <v-app-bar fixed bottom>
+        <v-text-field @keyup.enter="send()" v-model='field' label='Digite sua mensagem' solo hide-details></v-text-field>
+        <v-btn @click="send()" x-large icon>
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
       </v-app-bar>
     </v-main>
   </v-app>
 </template>
 
 <script>
+  import MessageCard from "./components/MessageCard.vue"
+  export default {
+    components: {
+      MessageCard
+    },
+    data() {
+      return {
+        name: 'João',
+        messages: [
+          { user: 'friend', text: 'bora' },
+          { user: 'me', text: 'opa' }
+        ],
+        field: ''
+      }
+    },
+    methods: {
+      send() {
+        let message = {
+          user: 'me',
+          text: this.field
+        }
+        this.messages.push(message)
+        this.field = ''
+      }
+    }
+  }
 </script>
 
 <style scoped>
-  
+  .messages-container {
+    width: 100%;
+    padding-top: 64px;
+    padding-bottom: 56px;
+  }
 </style>
