f07607cfd00cce8f8c3801820cf2e659d8d43d46
Filipe Antonio Cumaru Silva Alves <filipe-antonio.cumaru-silva-alves@etu.utc.fr>
2020-08-25 14:55:38 -0300

    Add vue files for the tutorial project.

---
+++ dia-2/beyond-school-tutorial/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar>
+        <v-btn icon x-large>
+          <v-icon x-large>mdi-chevron-left</v-icon>
+        </v-btn>
+        Filipe Cumaru
+      </v-app-bar>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  
+</style>
