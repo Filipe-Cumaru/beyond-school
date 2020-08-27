bd5eb5ee0196cfe02de0cdee2e866765101fe38d
Filipe Antonio Cumaru Silva Alves <filipe-antonio.cumaru-silva-alves@etu.utc.fr>
2020-08-27 11:40:04 -0300

    Add vue router boilerplate.

--- timeline-project/src/main.js
+++ timeline-project/src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuex)
+Vue.use(VueRouter)
+
+const vuexPersist = new VuexPersistence({
+  key: 'fhir_viwer_', // EDIT ME
+  storage: window.localStorage,
+  modules: [
+    'auth',
+    'common'
+  ]
+})
 
 const store = new Vuex.Store({
   state: {
@@ -19,10 +31,23 @@ const store = new Vuex.Store({
   },
   mutations: {
     
+  },
+  plugins: [vuexPersist.plugin]
+})
+
+const routes = [
+  {
+    path: '/',
+    component: App
   }
+]
+
+const router = new VueRouter({
+  routes
 })
 
 new Vue({
+  router,
   store,
   vuetify,
   render: h => h(App)
