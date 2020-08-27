3db072f2d7eb487a9d82fb95293443d1803199f7
Filipe Antonio Cumaru Silva Alves <filipe-antonio.cumaru-silva-alves@etu.utc.fr>
2020-08-27 10:56:44 -0300

    Add vuex boilerplate.

--- timeline-project/src/main.js
+++ timeline-project/src/main.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    
+  },
+  getters: {
+
+  },
+  actions: {
+
+  },
+  mutations: {
+    
+  }
+})
+
 new Vue({
+  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
