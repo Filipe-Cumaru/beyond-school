7250253cff587b5ed57e75b2825997bff0d3a784
Filipe Antonio Cumaru Silva Alves <filipe-antonio.cumaru-silva-alves@etu.utc.fr>
2020-08-31 08:00:42 -0300

    Fix bad route when clicking the go back button on the status bar.

--- timeline-project/src/components/StatusBarCard.vue
+++ timeline-project/src/components/StatusBarCard.vue
@@ -6,7 +6,7 @@
             </v-container>
         </v-dialog>
         <v-btn icon v-if="enableGoBackButtonProp">
-            <v-icon @click='$router.push("/")'>mdi-arrow-left</v-icon>
+            <v-icon @click='$router.push("/mainpage")'>mdi-arrow-left</v-icon>
         </v-btn>
         <v-icon>mdi-cloud</v-icon>
         <v-card-title>BeyondTimeline</v-card-title>
